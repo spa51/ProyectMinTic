f99bb66886ea66e3b92746a7580f14a9fdad5ff2
Ingcruzco <cruzco2009@hotmail.com>
2021-10-18 17:15:42 -0500

    backend first commit

---
+++ server/config/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDVcssK1V7n4jndI1Ro1wTfUgEJUsbrQVg",
+  authDomain: "misiontic-bb459.firebaseapp.com",
+  projectId: "misiontic-bb459",
+  storageBucket: "misiontic-bb459.appspot.com",
+  messagingSenderId: "809204228953",
+  appId: "1:809204228953:web:93483ddc0bdb61031ba248",
+  measurementId: "G-MZF7ZEGYSK"
+};
+
+module.exports=firebaseConfig;
